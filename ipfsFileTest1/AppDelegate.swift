82f12bf93f217c0aa1244dac807e6ffab9234509
Miaoshi Wu <miaoshiwu@miaoshis-mbp.home>
2018-03-28 00:20:10 -0400

    Initial Commit

---
+++ ipfsFileTest1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ipfsFileTest1
+//
+//  Created by Miaoshi Wu on 3/28/18.
+//  Copyright © 2018 Miaoshi Wu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ipfsFileTest1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ipfsFileTest1
+//
+//  Created by Miaoshi Wu on 3/28/18.
+//  Copyright © 2018 Miaoshi Wu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
