a7fa5e2b035c19e58a23c25450c8ad9a8eef5da5
Miaoshi Wu <miaoshiwu@wifi-64-187-255-72.nyit.edu>
2018-03-29 16:50:10 -0400

    woo

--- ipfsFileTest1/ViewController.swift
+++ ipfsFileTest1/ViewController.swift
@@ -16,12 +16,28 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var _userName: NSTextField!
+    @IBOutlet weak var _password: NSSecureTextField!
+    @IBOutlet weak var _signUp_button: NSButton!
+    @IBOutlet weak var _login_button: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        let UserName = _userName
+        let Password = _password
+        if (UserName == "" || Password == "")
+        {
+            return
+        }
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
