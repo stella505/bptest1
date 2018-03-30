b352c8b2b4e1e3ad391342719179dab3c6b085de
Miaoshi Wu <miaoshiwu@miaoshis-macbook-pro.local>
2018-03-29 20:39:02 -0400

    wo

--- ipfsFileTest1/ViewController.swift
+++ ipfsFileTest1/ViewController.swift
@@ -23,19 +23,51 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let preferences = UserDefaults.standard
+        if(preferences.object(forKey: "session")!=nil)
+        {
+            LoginDone()
+        }else{
+            LoginToDo()
+        }
         // Do any additional setup after loading the view.
     }
 
     
     @IBAction func LoginButton(_ sender: Any) {
+        if(_login_button.titleLabel?.text == "Logout")
+        {
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            LoginToDo()
+        }
         let UserName = _userName
         let Password = _password
         if (UserName == "" || Password == "")
         {
             return
         }
+        DoLogin(UserName!,Password!)
+    }
+    func DoLogin(_ user:String,_ psw:String)
+    {
+        let url = URL(String:"http")
+        let session = UPLSession.shared
         
+        let request = NSMutableURLRequest(url:url!)
+        request.httpMethod = "POST"
+        let paramToSend = "username" + user + "&password=" + psw
+        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        let task = session.dataTask(with:request as URLRequest, completionHandler:
+        {
+            (data,response,error) in
+            guard let _:Data = data else
+            {
+                return
+            }
+            
+        })
     }
     
     override var representedObject: Any? {
